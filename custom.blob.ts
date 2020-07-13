f5121383c86b84349ce3f2f759f5d0d5e1e6433d
shakao <shakao@microsoft.com>
2020-07-13 14:20:45 -0700

    Run buildsprites

---
+++ custom.blob.ts
@@ -0,0 +1,6 @@
+namespace custom.blob {
+    //% fixedInstance jres blockIdentity=undefined
+    export const blob1 = undefined(hex``);
+    //% fixedInstance jres blockIdentity=undefined
+    export const blob2 = undefined(hex``);
+}
